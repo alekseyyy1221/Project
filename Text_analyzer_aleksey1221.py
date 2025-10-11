939136686779af7eadecce72c7e8990db68c37d9
aleksey <bozdyxtadam@yandex.ru>
2025-10-11 15:22:17 +0300

    Теперь ещё и сохранил файл

---
+++ Text_analyzer_aleksey1221.py
@@ -0,0 +1,8 @@
+def count_words (text):
+#Подсчитывает количество слов в тексте
+    words = text.split()
+    return len (words)
+def count_lines (text):
+#Подсчитывает количество строк в тексте
+    Lines = text.split('\\n')
+    return len(lines)
