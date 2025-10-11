7fea0d877a5ff7d89bd956b088b52e085e6cd2cb
aleksey <bozdyxtadam@yandex.ru>
2025-10-11 16:26:32 +0300

    Новая функция

--- Text_analyzer_aleksey1221.py
+++ Text_analyzer_aleksey1221.py
@@ -6,3 +6,12 @@ def count_lines (text):
 #Подсчитывает количество строк в тексте
     Lines = text.split('\\n')
     return len(lines)
+def analyze_sentiment(text):
+#\"\"\"Анализирует тональность текста\"\"\"
+    positive_words = ['хорошо', 'отлично', 'прекрасно']
+    negative_words = ['плохо', 'ужасно', 'отвратительно']
+    score = 0
+    for word in text.lower().split():
+        if word in positive_words:
+            score += 1
+# Пока не завершено -нужно добавить negative words
